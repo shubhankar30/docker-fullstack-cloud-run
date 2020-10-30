36c56342fd7f5c2a4e17d9f9b1ecddf7304333e3
Shubhankar Ranade <shubhankarranade30@gmail.com>
2020-10-30 15:48:41 +0530

    Cloudbuild deployment

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -37,7 +37,11 @@ class Fib extends Component {
   };
 
   renderSeenIndexes() {
-    return this.state.seenIndexes.map(number => number).join(', ');
+    if(Array.isArray(this.state.seenIndexes) && this.state.seenIndexes.length > 0) {
+      return this.state.seenIndexes.map(number => number).join(', ');
+    } else {
+      return ''
+    }
   }
 
   renderValues() {
